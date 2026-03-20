ab3c1d0bb53ffc14bf565b4cf3d63409211b7c08
Ashish Yadava <ashish.yadav@qwings.in>
2026-03-20 11:09:56 +0530

    add trigger line in Main

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hello Sanjivani Students");
         System.out.println("Hello World!");
+        System.out.println("Checking for trigger");
     }
 
 }
